5d81e1618d1bf029ca8affc0bc865514d096194d
aaron-everly <aaron.everly@legitscript.com>
2024-03-03 17:35:08 -0800

    add progress

---
+++ go.mod
@@ -0,0 +1,23 @@
+module app
+
+go 1.22.0
+
+require (
+	github.com/jackc/pgx/v4 v4.18.1
+	github.com/joho/godotenv v1.5.1
+	github.com/shopspring/decimal v1.3.1
+	github.com/steebchen/prisma-client-go v0.35.0
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+)
+
+type Poll struct {
+	ID          string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string
+	Published   bool
+	Description string
+	Votes       int
+}
+
+func main() {
+	var port = ":8080"
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var polls []Poll
+	rows, err := conn.Query(context.Background(), `SELECT id, title, published, "createdAt", "updatedAt", votes, description FROM "Poll"`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var poll Poll
+		if err := rows.Scan(&poll.ID, &poll.Title, &poll.Published, &poll.CreatedAt, &poll.UpdatedAt, &poll.Votes, &poll.Description); err != nil {
+			fmt.Fprintf(os.Stderr, "Scan failed: %v\n", err)
+			os.Exit(1)
+		}
+		polls = append(polls, poll)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error iterating through rows: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Connected!")
+	fmt.Println("Polls:", polls)
+
+	pageData := struct {
+		Polls []Poll
+	}{
+		Polls: polls,
+	}
+
+	serveHandler := func(w http.ResponseWriter, r *http.Request) {
+		templ := template.Must(template.ParseFiles("index.html"))
+		if err := templ.Execute(w, pageData); err != nil {
+			log.Printf("Error executing template: %v", err)
+			http.Error(w, "Error generating page", http.StatusInternalServerError)
+		}
+	}
+
+	http.HandleFunc("/", serveHandler)
+
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
